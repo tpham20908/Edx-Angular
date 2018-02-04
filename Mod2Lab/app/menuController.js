416354e5e1c3b4dfcc3b4bd886fcc4a34bd950f5
unknown <tpham20908@gmail.com>
2018-02-04 18:26:31 -0500

    Create custom $watch()

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -2,6 +2,11 @@ app.controller('menuController', [
     '$scope',
     function($scope) {
         $scope.model = {title: 'Our Menu'};
+        $scope.$watch('model.mainDish', function(newValue, oldValue) {
+            if (oldValue === 'BBQ Chicken Pizza') {
+                alert('Why don\'t you select BBQ Chicken Pizza');
+            }
+        });
         $scope.changeMainDish = function(item) {
             $scope.model.mainDish = item;
         }
