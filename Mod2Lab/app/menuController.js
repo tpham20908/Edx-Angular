dbc5dde61cf17f1cab879398194d4df24dfd05dc
unknown <tpham20908@gmail.com>
2018-02-04 20:54:57 -0500

    finish Mod2Lab

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -1,14 +1,18 @@
 app.controller('menuController', [
     '$scope',
     function($scope) {
-        $scope.model = {title: 'Our Menu'};
-        $scope.$watch('model.mainDish', function(newValue, oldValue) {
+        $scope.model = {
+            title: 'Our Menu',
+            mainDish: {}
+        };
+        $scope.$watch('model.mainDish.name', function(newValue, oldValue) {
             if (oldValue === 'BBQ Chicken Pizza') {
                 alert('Why don\'t you select BBQ Chicken Pizza');
             }
         });
-        $scope.changeMainDish = function(item) {
-            $scope.model.mainDish = item;
+        $scope.changeMainDish = function(item, price) {
+            $scope.model.mainDish.name = item;
+            $scope.model.mainDish.price = '$' + price;
         }
     }
 ]);
